ed834de64a2a6edbc18c3864e5fb7f67970b12cc
BuildTools <unconfigured@null.spigotmc.org>
2022-09-09 14:10:40 -0300

    Quase concluindo o projeto

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,9 +1,9 @@
-#include <string>
+#include <cstring>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
-
 struct fornecedores {
     string nome;
     string email;
@@ -20,66 +20,8 @@ struct produtos {
     string situacao;
 };
 
-void cadastrarProduto (produtos &produto) {
-    //preencher os dados do produto
-    cout << "Descrição: ";
-    getline (cin, produto.descricao);
-    cout << "Código: ";
-    getline (cin, produto.codigo);
-    cout << "Quantidade: ";
-    cin >> produto.quantidadeDisp;
-    cout << "Valor: ";
-    getline (cin, produto.valor);
-    cout << "Fornecedor" << endl << "Nome: ";
-    getline (cin, produto.fornecedor.nome);
-    cout << "Email: ";
-    getline (cin, produto.fornecedor.email);
-    cout << "Telefone: ";
-    getline (cin, produto.fornecedor.telefone);
-    produto.situacao = "ativo";
-    cout << "Produto cadastrado com sucesso!" << endl;
-}
-
-void checaProduto (produtos produto, bool &existe) {
-    //checar se o codigo do produto ja existe
-    ifstream arquivo;
-    arquivo.open("produtos.bin");
-    string linha;
-    while (getline(arquivo, linha)) {
-        if (linha == produto.codigo) {
-            existe = true;
-            cout << "Produto já cadastrado!" << endl;
-            break;
-        }
-    }
-    arquivo.close();
-}
-
-void exportarProduto(produtos produto) {
-    //exportar em binario
-    ofstream arquivo;
-    arquivo.open("produtos.bin", ios::binary | ios::app);
-    arquivo.write((char*)&produto, sizeof(produto));
-    arquivo.close();
-}
-
-void consultarProduto (produtos produto) {
-    //consultar um produto pelo codigo
-    ifstream arquivo;
-    arquivo.open("produtos.bin", ios::binary);
-    while (arquivo.read((char*)&produto, sizeof(produto))) {
-        if (produto.codigo == produto.codigo) {
-            cout << "Código: " << produto.codigo << endl;
-            cout << "Quantidade: " << produto.quantidadeDisp << endl;
-            cout << "Quantidade vendida: " << produto.quantidadeVendida << endl;
-            cout << "Situação: " << produto.situacao << endl;
-        }
-    }
-    arquivo.close();
-}
-
-
 int menu () {
+    //menu do programa
     int opcao;
     cout << "1 - Cadastrar produto" << endl;
     cout << "2 - Consultar produtos" << endl;
@@ -91,46 +33,249 @@ int menu () {
     cout << "8 - Sair" << endl;
     cout << "Opção: ";
     cin >> opcao;
+    cin.ignore();
     return opcao;
+}
+
+void aumentaVetor (produtos *produto, int &tamanho) {
+    //função que aumenta o vetor produto em 3
+    produtos *aux = new produtos[tamanho+3];
+    for (int i = 0; i < tamanho; i++) 
+        aux[i] = produto[i];
+    delete [] produto;
+    produto = aux;
+    tamanho += 3;
+}
+
+bool checaProduto (string codigo, produtos produto[], int tam) {
+    //função que checa se o codigo do produto já existe no vetor produto
+    for (int i = 0; i < tam; i++) {
+        if (codigo == produto[i].codigo) 
+            return true;
+    }
+    return false;
+}
+
+
+void cadastrarProduto (produtos produto[], int p, int tam) {
+    //função que preenche os dados do produto
+    string codigo;
+    cout << "Descrição: ";
+    getline (cin, produto[p].descricao);
+    cout << "Código: ";
+    getline (cin, codigo);
+    //checa se o codigo já existe
+    if (checaProduto (codigo, produto, tam) == false) {
+        produto[p].codigo = codigo;
+        cout << "Quantidade disponível: ";
+        cin >> produto[p].quantidadeDisp;
+        cin.ignore();
+        cout << "Valor: ";
+        getline (cin, produto[p].valor);
+        cout << "Fornecedor" << endl << "Nome: ";
+        getline (cin, produto[p].fornecedor.nome);
+        cout << "Email: ";
+        getline (cin, produto[p].fornecedor.email);
+        cout << "Telefone: ";
+        getline (cin, produto[p].fornecedor.telefone);
+        produto[p].situacao = "ativo";
+        cout << "Produto cadastrado com sucesso!" << endl;
+    }
+    //caso o codigo do produto já exista, o produto não é cadastrado
+    else
+        cout << "Produto já cadastrado!" << endl; 
+}
+
+void quickSort (produtos produto[], int inicio, int fim) {
+    //função que ordena o vetor produto pelo código do produto, usando o método quick sort
+    int i = inicio;
+    int j = fim;
+    //define o pivô
+    string pivo = produto[(inicio + fim) / 2].codigo;
+    while (i <= j) {
+        //encontra o primeiro elemento maior que o pivô
+        while (produto[i].codigo < pivo && i < fim) 
+            i++;
+        //encontra o primeiro elemento menor que o pivô
+        while (produto[j].codigo > pivo && j > inicio) 
+            j--;
+        //troca os elementos
+        if (i <= j) {
+            produtos aux = produto[i];
+            produto[i] = produto[j];
+            produto[j] = aux;
+            i++;
+            j--;
+        }
+    //chama a função novamente para ordenar as duas metades
+    }
+    if (j > inicio) 
+        quickSort (produto, inicio, j);
+    if (i < fim) 
+        quickSort (produto, i, fim);
+}
+
+void consultarProduto (produtos produto[], int tam) {
+    //função que consulta um produto pelo código
+    string c;
+    cout << "Código: ";
+    getline (cin, c);
+    for (int i = 0; i < tam; i++) {
+        if (c == produto[i].codigo) {
+            cout << "Quantidade disponível: " << produto[i].quantidadeDisp << endl;
+            cout << "Quantidade vendida: " << produto[i].quantidadeVendida << endl;
+            cout << "Situação: " << produto[i].situacao << endl;
+        }
+    }
+}
+
+void listarProdutosDisponiveis (produtos produto[], int tam) {
+    //função que lista os produtos disponíveis ordenados pelo código do produto
+    //chama a função quickSort para ordenar o vetor produto
+    quickSort (produto, 0, tam-1);
+    for (int i = 0; i < tam; i++) {
+        //se o produto estiver disponível(ter pelo menos 1 produto para vender), ele é listado
+        if (produto[i].quantidadeDisp > 0) {
+            cout << "Descrição: " << produto[i].descricao << endl;
+            cout << "Código: " << produto[i].codigo << endl;
+            cout << "Fornecedor" << endl << "Nome: " << produto[i].fornecedor.nome << endl;
+            cout << "Email: " << produto[i].fornecedor.email << endl;
+            cout << "Situação: " << produto[i].situacao << endl;
+        }
+    }
+}
+
+void excluirProduto (produtos produto[], int tam) {
+    //função que exclui um produto pela exclusão lógica
+    string codigo;
+    cout << "Código do produto: ";
+    getline (cin, codigo);
+    for (int i = 0; i < tam; i++) {
+        if (codigo == produto[i].codigo) {
+            cout << produto[i].descricao << endl << "Deseja realmente excluir o produto? 1-sim 2-nao" << endl;
+            int opcao;
+            cin >> opcao;
+            cin.ignore();
+            if (opcao == 1) {
+                produto[i].situacao = "inativo";
+                cout << "Produto excluído com sucesso!" << endl;
+            }
+            else
+                cout << "Produto não excluído!" << endl;
+        }
+    }
+    //os produtos excluídos não são removidos do vetor, apenas a situação que é alterada para inativo
+}
+
+void vendaProduto (produtos produto[], int tam) {
+    //função que realiza uma venda
+    string codigo;
+    int quantidade;
+    cout << "Código do produto: ";
+    getline (cin, codigo);
+    for (int i = 0; i < tam; i++) {
+        if (codigo == produto[i].codigo) {
+            cout << "Quantidade: ";
+            cin >> quantidade;
+            cin.ignore();
+            //se a quantidade que o usuário quer vender for maior que a quantidade disponível, a venda não é realizada
+            if (quantidade <= produto[i].quantidadeDisp) {
+                produto[i].quantidadeDisp -= quantidade;
+                produto[i].quantidadeVendida += quantidade;
+                cout << "Quantidade vendida: " << produto[i].quantidadeVendida << endl;
+                cout << "Quantidade disponível: " << produto[i].quantidadeDisp << endl;
+                cout << "Venda realizada com sucesso!";
+            }
+            else
+                cout << "Quantidade indisponível!" << endl;
+        }
+    }
+}
+
+void listarProdutosAtivos (produtos produto[], int tam) {
+    //função que lista os produtos cadastrados ordenados em quick sort se eles estiverem ativos
+    //chama a função quickSort para ordenar o vetor produto
+    quickSort (produto, 0, tam - 1);
+    // imprime os produtos ativos
+    for (int i = 0; i < tam; i++) {
+        if (produto[i].situacao == "ativo") {
+            cout << "Código: " << produto[i].codigo << endl;
+            cout << "Descrição: " << produto[i].descricao << endl;
+            cout << "Quantidade disponível: " << produto[i].quantidadeDisp << endl;
+            cout << "Quantidade vendida: " << produto[i].quantidadeVendida << endl;
+            cout << "Valor: " << produto[i].valor << endl;
+            cout << "Fornecedor" << endl << "Nome: " << produto[i].fornecedor.nome << endl;
+            cout << "Email: " << produto[i].fornecedor.email << endl;
+            cout << "Telefone: " << produto[i].fornecedor.telefone << endl;
+            cout << "Situação: " << produto[i].situacao << endl;
+        }
+    }
+}
+
+void exportarProduto(produtos produto[], int tam) {
+    //exportar em binario
+    ofstream arquivo;
+    arquivo.open("produtos.dat");
+    if (arquivo.is_open()) {
+        arquivo.write((char *)&produto, tam*sizeof(produtos));
+    }
+    //ler o arquivo
+    ifstream arquivo2;
+    arquivo2.open("produtos.dat");
+    if (arquivo2.is_open()) {
+        arquivo2.read((char *)&produto, tam*sizeof(produtos));
+    }
+    arquivo.close();
+    arquivo2.close();
 }
 
 int main () {
     //menu da loja
-    produtos produto;
-    bool existe = false;
-    int opcao = menu();
+    int i = 0, t = 3, opcao;
+    produtos *produto = new produtos[t];
+    string c;
+    opcao = menu();
     while (opcao != 8) {
         switch (opcao) {
             case 1:
-            checaProduto(produto, existe);
-                if (existe == false) 
-                    cadastrarProduto (produto);
-                else 
-                    existe = false;
+                //se o vetor produtos estiver lotado, o programa deve aumentar o tamanho do vetor em 3
+                if (i == t) 
+                    aumentaVetor(produto, t);
+                cout << "Cadastrar produto" << endl;
+                cadastrarProduto (produto, i, t);
                 break;
             case 2:
-                consultarProduto (produto);
+                //consultar um produto pelo codigo
+                consultarProduto (produto, t);
                 break;
             case 3:
+                //listar produtos cadastrados disponíveis ordenados pelo código do produto
+                listarProdutosAtivos (produto, t);
                 break;
             case 4:
+                //excluir produto
+                excluirProduto (produto, t);
                 break;
             case 5:
+                //efetuar uma venda
+                vendaProduto (produto, t);
                 break;
             case 6:
+                //listar dados de produtos ativos em estoque
+                listarProdutosAtivos (produto, t);
                 break;
             case 7:
-                exportarProduto(produto);
-                break;
-            case 8:
-                cout << "Encerrando sistema..." << endl;
-                return 0
+                exportarProduto (produto, t);
                 break;
             default:
                 cout << "Opção inválida" << endl;
                 break;
-        }
-        int opcao = menu();
+            }
+            if (opcao == 1)
+                i++;
+            if (opcao != 8)
+                opcao = menu();
     }
+    cout << "Encerrando sistema..." << endl;
     return 0;
 }
